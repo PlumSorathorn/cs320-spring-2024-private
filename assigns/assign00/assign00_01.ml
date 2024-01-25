247955b751112f61685f1506b47a4f865a9115e7
Sorathorn Thongpitukthavorn <plum@bu.edu>
2024-01-25 17:30:24 -0500

    done

--- assigns/assign00/assign00_01.ml
+++ assigns/assign00/assign00_01.ml
@@ -17,5 +17,9 @@
 
 *)
 
-let sqrt (n : int) : int =
-  (* YOUR CODE GOES HERE *)
+let sqrt (n : int) : int = 
+   (* YOUR CODE GOES HERE *)
+   if sqrt(float_of_int(n)) /. 1.0 != 0.00 then truncate(sqrt(float_of_int(n))) + 1
+   else truncate(sqrt(float_of_int(n)));;
+
+let _ = print_int(sqrt 99);;
--- assigns/assign00/assign00_02.ml
+++ assigns/assign00/assign00_02.ml
@@ -3,7 +3,6 @@
    Please implement a function `is_prime` of type `int -> bool` such that
    when given an integer `n` greater than or equal to 0, it returns a boolean value.
    If the input is prime return true, otherwise return false.
-
    Note 1: 0 and 1 are not considered prime numbers.
    Note 2: In OCaml, the modulo operation is written a `4 mod 3`. This specific instance will result in 1. 
 
@@ -16,6 +15,14 @@
    is_prime 97 = true
 
 *)
-
+let rec check (p : int) (i : int) : bool = 
+   if i * i > p then true
+   else if p mod i = 0 then false
+   else check (p)(i + 1);;
 let is_prime (n : int) : bool =
   (* YOUR CODE GOES HERE *)
+  if n <= 1 then false 
+  else if check(n)(2) then true else false;;
+
+let _ = print_endline (string_of_bool (is_prime 37));;
+   
