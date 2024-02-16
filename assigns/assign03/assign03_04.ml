97e95064da82d52533857221040cb03374966e25
Sorathorn Thongpitukthavorn <plum@bu.edu>
2024-02-15 22:30:27 -0500

    4 possibly done

--- assigns/assign03/assign03_04.ml
+++ assigns/assign03/assign03_04.ml
@@ -115,11 +115,75 @@ type 'a matrix = {
   rows : ('a list) list ;
 }
 
+(* helper functions *)
+
+let rec length lst =
+  match lst with
+  | [] -> 0
+  | n :: rest -> 1 + length rest;;
+
+let rec get_i lst index =
+  match lst with
+  | [] -> assert false
+  | head :: tail -> if index = 0 then head else get_i tail (index - 1);;
+(* end of helper functions *)
+  
 let mkMatrix (rs : 'a list list) : ('a matrix, error) result =
-  assert false (* TODO *)
+  let rec count_i lst acc = 
+    match lst with
+    | [] -> acc
+    | n :: rest -> count_i rest (acc + 1) 
+  in let rec same_length lsts len = 
+    match lsts with
+    | [] -> true
+    | n :: rest -> (count_i n 0 = len) && same_length rest len 
+  in let rec create_matrix lsts = 
+    match lsts with
+    | [] -> Error ZeroRows
+    | [[]] -> Error ZeroCols
+    | n :: rest ->
+      let n_length = count_i n 0 in
+      if n_length = 0 then Error ZeroCols
+      else if same_length rest n_length then Ok { num_rows = count_i lsts 0; num_cols = n_length; rows = lsts }
+      else Error UnevenRows 
+    in create_matrix rs;;
 
 let transpose (m : 'a matrix) : 'a matrix =
-  assert false (* TODO *)
+  let rec transpose_col rows col_i =
+    match rows with
+    | [] -> []
+    | r :: rest ->
+      if col_i < length r then
+        (get_i r col_i) :: transpose_col rest col_i
+      else []
+    in let rec transposer m col_index =
+    if col_index < m.num_cols then
+      transpose_col m.rows col_index :: transposer m (col_index + 1)
+    else []
+    in let t_rows = transposer m 0 
+  in { num_rows = m.num_cols; num_cols = m.num_rows; rows = t_rows };;
+
+
+let dot row col =
+  let rec dot_loop r c acc =
+    match r, c with
+    | [], [] -> acc
+    | r :: r_rest, c :: c_rest -> dot_loop r_rest c_rest (acc +. r *. c)
+    | _, _ -> acc
+  in dot_loop row col 0.0;;
 
 let multiply (m : float matrix) (n : float matrix) : (float matrix, error) result =
-  assert false (* TODO *)
+  if m.num_cols <> n.num_rows then Error MulMismatch
+  else
+    let transposed_n = transpose n in
+    let rec times_row m_row n_col =
+      match m_row with
+      | [] -> []
+      | m_row :: m_rest ->
+          let rec times_col n_columns =
+            match n_columns with
+            | [] -> []
+            | n_col :: n_rest -> (dot m_row n_col) :: (times_col n_rest)
+          in (times_col n_col) :: (times_row m_rest n_col)
+      in let product_rows = (times_row m.rows transposed_n.rows) 
+    in Ok { num_rows = m.num_rows; num_cols = n.num_cols; rows = product_rows };;
